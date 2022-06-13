04aec03a21dddd1d349b31061c3a2dd28653e141
Ian de Aquino Bianquini <ian.aquino2003@gmail.com>
2022-06-13 17:17:19 -0300

    feat(Product): Header and Photo

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,12 +1,28 @@
+import { ButtonBack } from "@components/ButtonBack"
+import { Photo } from "@components/Photo"
 import React from "react"
 import { Platform } from "react-native"
-import { Container } from "./styles"
+import { TouchableOpacity } from "react-native-gesture-handler"
+import { Container, DeleteLabel, Header, PickImageButton, Title, Upload } from "./styles"
 
 
-export function Product (){
+export default function Product (){
   return(
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      
+      <Header>
+        <ButtonBack activeOpacity={0.6}/>
+        <Title>Cadastrar</Title>
+
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+      </Header>
+
+      <Upload>
+        <Photo uri={""} />
+        
+        <PickImageButton title="Carregar" type="secondary" />
+      </Upload>
     </Container>
   )
 }
